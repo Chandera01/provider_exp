055068779efe4c42fc467ac1a172bb289c93f4f5
Kinjal <kinjalchandera0111@gmail.com>
2024-11-26 21:15:24 +0530

    second page provider

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,12 @@ import 'package:provider/provider.dart';
 import 'package:provider_exp/counter_provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(ChangeNotifierProvider(
+    create: (context) => CounterProvider(),
+    child: MyApp(),
+  )
+
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -18,9 +23,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: ChangeNotifierProvider(create: (_) => CounterProvider(),
-      child: MyHomePage(),
-      ),
+      home: MyHomePage(),
     );
   }
 }
@@ -28,7 +31,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
         title: Text("Provider"),
@@ -37,7 +39,6 @@ class MyHomePage extends StatelessWidget {
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Column(
-
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
@@ -45,20 +46,40 @@ class MyHomePage extends StatelessWidget {
             ),
             Text(
               ///data reflection listener
-              '${Provider.of<CounterProvider>(context,listen: true).getCountValue()}',
+              '${Provider.of<CounterProvider>(context, listen: true).getCountValue()}',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){
-              ////Increment
+        onPressed: () {
+          ////Increment
           ////Data changes
-          Provider.of<CounterProvider>(context,listen: false).incrementCount();
+          // Provider.of<CounterProvider>(context,listen: false).incrementCount();  ///this for same page increment
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => Secondpage()));
         },
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class Secondpage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Second page"),
+      ),
+      body: Center(
+        child: ElevatedButton(
+            onPressed: () {
+              Provider.of<CounterProvider>(context, listen: false).incrementCount();
+            },
+            child: Icon(Icons.add)),
+      ),
     );
   }
 }
